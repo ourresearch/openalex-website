28db3f6280fe1bdc68c102339b8155bc983b1d28
Jason Priem <jp@jasonpriem.org>
2021-11-14 21:08:23 -0800

    contact page done.

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,20 @@
 <template>
 <v-container class="page">
-  <h2 class="text-h3">Contact us</h2>
+  <h2 class="text-h3 pb-6">Contact us</h2>
+  
+  <p>
+  We'd love to hear from you! Drop us an email at <a
+      href="mailto:team@ourresearch.org">team@ourresearch.org</a> any time.
+  </p>
 
+  <p>
+You can also stay in touch with us via the <a
+      href="http://eepurl.com/hA8PhL">OpenAlex mailing list,</a> and on Twitter at <a
+      href="https://twitter.com/openalex_org">@OpenAlex_org.</a>
+  </p>
+
+  
+  
 
 </v-container>
 </template>
