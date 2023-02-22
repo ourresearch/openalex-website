5265848d01a1405f4e1f85a462445b480936e2cf
h1-the-swan <jason.portenoy@gmail.com>
2023-02-22 13:33:42 -0500

    add link to api to get all sources

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,17 +9,20 @@
 
       Using OpenAlex, you can <strong>build</strong> your own scholarly search engine, recommender service, or knowledge
       graph. You can help <strong>manage</strong> research by tracking citation impact, spotting promising new research
-      areas, and identifying and promoting work from underrepresented groups. And you can do <strong>research</strong> on research
+      areas, and identifying and promoting work from underrepresented groups. And you can do <strong>research</strong> on
+      research
       itself, in areas like
       <a href="https://en.wikipedia.org/wiki/Bibliometrics">bibliometrics</a>,
       <a href="https://en.wikipedia.org/wiki/Science_and_technology_studies">science and technology studies</a>, and
       <a href="https://en.wikipedia.org/wiki/Science_of_science_policy">Science of science policy</a>.
     </p>
     <p>
-      Because we think all research should be free and open, OpenAlex is free and open itself, and we're built on a fully <a href="https://github.com/ourresearch/openalex-guts">Open Source codebase.</a>
+      Because we think all research should be free and open, OpenAlex is free and open itself, and we're built on a fully
+      <a href="https://github.com/ourresearch/openalex-guts">Open Source codebase.</a>
     </p>
     <p>
-      We believe the global research system is one of humankind's most beautiful creations. OpenAlex aims to make that whole beautiful creation available to everyone, everywhere.
+      We believe the global research system is one of humankind's most beautiful creations. OpenAlex aims to make that
+      whole beautiful creation available to everyone, everywhere.
     </p>
 
 
@@ -40,17 +43,18 @@
         href="https://www.arcadiafund.org.uk/">Arcadia, a charitable fund of Lisbet Rausing and Peter Baldwin.</a>
       Second, Microsoft announced it would shutter <a
         href="https://www.microsoft.com/en-us/research/project/microsoft-academic-graph/">Microsoft Academic Graph
-      (MAG),</a> which much of the community has come to rely upon as our best index of scholarly communication. So, we
+        (MAG),</a> which much of the community has come to rely upon as our best index of scholarly communication. So, we
       had the means and the opportunity, and we ran with it!
     </p>
     <p>
       OpenAlex beta launched on January 3rd, 2022. Currently the data is available via an <a
         href="https://docs.openalex.org/">API</a> and a full <a
-        href="https://docs.openalex.org/download-all-data/openalex-snapshot">database snapshot.</a> We also have plans to create a web-based graphical user interface.
+        href="https://docs.openalex.org/download-all-data/openalex-snapshot">database snapshot.</a> We also have plans to
+      create a web-based graphical user interface.
     </p>
     <p>
       If you're interested in following our progress, we encourage you to <a href="http://eepurl.com/hA8PhL">join our
-      mailing list!</a>
+        mailing list!</a>
     </p>
 
     <h4 class="text-h4" id="sources">Sources</h4>
@@ -73,6 +77,10 @@
           href="https://zenodo.org">Zenodo</a> and everywhere in between.
       </li>
     </ul>
+    <p>
+      You can get the full list of sources <a
+        href="https://docs.openalex.org/api-entities/venues/get-lists-of-venues">using our API.</a>
+    </p>
 
     <h4 class="text-h4" id="pricing">Pricing</h4>
     <p>
@@ -81,8 +89,8 @@
 
     <p class="mt-4">
       We’re currently exploring sustainability models around premium access (like a daily update feed, or a
-      high-throughput API). We’re proud that our other projects (<a
-        href="https://unsub.org">Unsub</a> and <a href="https://unpaywall.org">Unpaywall</a>) have both been
+      high-throughput API). We’re proud that our other projects (<a href="https://unsub.org">Unsub</a> and <a
+        href="https://unpaywall.org">Unpaywall</a>) have both been
       self-sustaining for years, and we think that’s vital for OpenAlex (and other scholarly infrastructure projects) as
       well.
 
@@ -91,7 +99,8 @@
 
     <h4 class="text-h4" id="acknowledgements">Acknowledgements</h4>
     <p>
-      We'd like to thank everyone behind all our <a href="#sources">data sources,</a> but especially the folks at <a href="https://aka.ms/msracad">MAG</a>
+      We'd like to thank everyone behind all our <a href="#sources">data sources,</a> but especially the folks at <a
+        href="https://aka.ms/msracad">MAG</a>
       for building a really audacious and cool thing, and providing us lots of help and support as we build its
       replacement.
     </p>
@@ -103,13 +112,10 @@
 <script>
 export default {
   name: "About",
-  metaInfo: {title: "About"}
+  metaInfo: { title: "About" }
 }
 </script>
 
-<style scoped lang="scss">
-.text-h4 {
+<style scoped lang="scss">.text-h4 {
   padding-top: 50px;
-}
-
-</style>
+}</style>
