61008f7af40d2181fe3fdeb0c8a10cf163d87157
h1-the-swan <jason.portenoy@gmail.com>
2023-09-21 20:24:06 +0000

    remove redirects for openalex entities

--- server.js
+++ server.js
@@ -14,62 +14,8 @@ app.use(sslRedirect(['production'], 301));
 app.use(serveStatic(__dirname + "/dist"));
 
 
-const entityTypes = {
-    "W": "works",
-    "I": "institutions",
-    "V": "venues",
-    "A": "authors",
-    "C": "concepts",
-};
-
-
-// match these:
-// /w123.json
-// /w123
-// /W123
-//
-// // miss me with these:
-// /w123.html
-// /123
-// /u123
-const openAlexIdRegex = /^\/([wWiIvVaAcC]\d+)(\.json)?$/;
-
-function requestPrefersJson(req) {
-    var acceptTypes = {}
-
-    for (const acceptPair of (req.headers.accept ?? "").split(",").map(a => a.split(";"))) {
-        const acceptType = acceptPair[0];
-        const acceptQuality = parseFloat((acceptPair[1] ?? "").split("q=")[1] ?? "1");
-        acceptTypes[acceptType] = isNaN(acceptQuality) ? 1 : acceptQuality;
-    }
-
-    const maxQuality = Math.max.apply(null, Object.values(acceptTypes));
-    const typesWithMaxQuality = Object.entries(acceptTypes).filter(e => e[1] >= maxQuality).map(e => e[0]);
-    return typesWithMaxQuality.some(t => t.toLowerCase().includes("json"));
-}
-
 app.get('*', function (req, res) {
-
-    const pathIdMatches = req.path.match(openAlexIdRegex)
-
-    // this is actually a PID that we need to resolve, not a page on this site.
-    // we can resolve it to the API, or the web UI
-    if (pathIdMatches) {
-        const pathEndsInDotJson = !!pathIdMatches[2]
-        const preferJson = requestPrefersJson(req);
-        const subdomain = (pathEndsInDotJson || preferJson) ? "api" : "explore"
-
-        const id = pathIdMatches[1]
-        const idFirstLetter = id.substr(0, 1).toUpperCase()
-        const entityType = entityTypes[idFirstLetter];
-
-        const redirectUrl = `https://${subdomain}.openalex.org/${entityType}/${id}`
-        res.redirect(301, redirectUrl);
-
-    // the actually do want a webpage on openalex.org, so let's load up the Vue site
-    } else {
-        res.sendFile('./dist/index.html', { root: __dirname });
-    }
+    res.sendFile('./dist/index.html', { root: __dirname });
 });
 
 const port = process.env.PORT || 5000;
